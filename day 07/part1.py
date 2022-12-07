dc6ac3f639763fd7ba514f1668473b5b9268e9e2
PaoDerDoktor <PaoDerDoktor@gmail.com>
2022-12-07 18:39:21 +0100

    feat: Solved Day 7

---
+++ day 07/part1.py
@@ -0,0 +1,71 @@
+def get_dir_size(dirTree: dict[str, set[str]], directDirSize: dict[str, int], finalSizes: dict[str, int], dir: str) -> int:
+    travelStack: list[str] = [dir]
+    visited:     set[str]  = set()
+    
+    while len(travelStack) != 0:
+        currentDir: str = travelStack[-1]
+        
+        expand: bool = False
+        
+        childrens: set[str] = dirTree[currentDir]
+        
+        for child in childrens:
+            if child not in visited:
+                travelStack.append(child)
+                expand = True
+        
+        if not expand:
+            finalSizes[currentDir] = directDirSize[currentDir] + sum([finalSizes[child] for child in childrens])
+            travelStack.pop()
+            visited.add(currentDir)
+
+    return finalSizes[dir]
+
+def day7_part1_main() -> int:
+    with open("day 07/inputs.txt", 'r') as inFile:
+        dirTree:       dict[str, set[str]] = {}
+        directDirSize: dict[str, int]      = {}
+        
+        pwd: str = '/' # currentPath
+        pc:  int = 0   # Current line
+        
+        lines: list[list[str]] = [l.strip().split() for l in inFile.readlines()]
+        
+        while pc != len(lines):
+            line: list[str] = lines[pc]
+            if line[0] == '$': # Line is a command
+                if line[1] == "cd": # Command is a directory change
+                    if line[2] == "/": # Program should go to root
+                        pwd = "/"
+                    elif line[2] == "..": # Program should go back
+                        lastBranch: int = -(pwd[::-1][1:].index('/')+1)
+                        pwd = pwd[:lastBranch]
+                    else: # Program should enter a new folder
+                        pwd += line[2] + "/"
+                    
+                    pc += 1
+                elif line[1] == "ls": # Command is a listing of content
+                    pc += 1
+                    
+                    if pwd not in dirTree:
+                        dirTree[pwd] = set()
+                    
+                    if pwd not in directDirSize:
+                        directDirSize[pwd] = 0
+                    
+                    while pc != len(lines) and lines[pc][0] != '$': # While not at EOF or next command...
+                        if lines[pc][0] == "dir": # Item is a folder
+                            dirTree[pwd].add(pwd+lines[pc][1]+"/")
+                        else: # Item is a file
+                            directDirSize[pwd] += int(lines[pc][0])
+                        pc += 1
+        
+        # Recursively compute folder sizes from direct sizes and dirTree
+        finalSizes: dict[str, int] = {k: -1 for k in directDirSize}
+        
+        rootSize: int = get_dir_size(dirTree, directDirSize, finalSizes, '/')
+        
+        return sum([finalSizes[dir] for dir in finalSizes if finalSizes[dir] <= 100000])                  
+    
+if __name__ == "__main__":
+    print(day7_part1_main())
